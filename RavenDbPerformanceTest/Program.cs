99c0ef82e49f943126bf584903a359f37b357691
bdrupieski <brian.drupieski@gmail.com>
2015-01-05 20:52:40 -0500

    add NDesk.Options and MiniProfiler, write output to file

--- RavenDbPerformanceTest/Program.cs
+++ RavenDbPerformanceTest/Program.cs
@@ -1,24 +1,50 @@
 ﻿using System;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading;
+using NDesk.Options;
 using Raven.Abstractions.Data;
 using Raven.Client;
 using Raven.Client.Embedded;
 using Raven.Client.Linq;
 using RavenDbPerformanceTest.Entities;
 using RavenDbPerformanceTest.Indexes;
+using StackExchange.Profiling;
 
 namespace RavenDbPerformanceTest
 {
-    class Program
+    internal class Program
     {
-        public static int IndexCount = 100;
-        public static int DocumentCount = 4000;
-
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            MiniProfiler.Settings.ProfilerProvider = new SingletonProfilerProvider();
+            MiniProfiler.Start("RavenDB Benchmark");
+
+            int indexCount = 100;
+            int documentCount = 4000;
+
+            var options = new OptionSet
+            {
+                { "i|indexes=", "number of indexes to create", v => indexCount = int.Parse(v) },
+                { "d|documents=", "number of documents*2 to create", v => documentCount = int.Parse(v) },
+            };
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage:");
+                options.WriteOptionDescriptions(Console.Out);
+                Console.WriteLine("Using defaults of {0} indexes and {1} documents", indexCount, documentCount);
+                Console.WriteLine();
+            }
+            else
+            {
+                options.Parse(args);
+            }
+            
+            Console.WriteLine("Running with {0} {1} and {2} {3}", 
+                indexCount, indexCount == 1 ? "index" : "indexes",
+                documentCount, documentCount == 1 ? "document" : "documents");
+
             var dir = new DirectoryInfo("Data");
             if (dir.Exists)
             {
@@ -32,48 +58,53 @@ namespace RavenDbPerformanceTest
                 UseEmbeddedHttpServer = true,
             };
 
-            using (SimpleProfiler.Start("Initializing DocumentStore"))
+            using (MiniProfiler.Current.Step("Initializing DocumentStore"))
             {
                 documentStore.Initialize();
             }
 
-            //using (SimpleProfiler.Start("Deleting all indexes"))
+            //using (MiniProfiler.Current.Step("Deleting all indexes"))
             //{
             //    DeleteAllIndexes(documentStore);
             //}
 
-            //using (SimpleProfiler.Start("Deleting all documents"))
+            //using (MiniProfiler.Current.Step("Deleting all documents"))
             //{
             //    DeleteAllDocuments<Client>(documentStore);
             //    DeleteAllDocuments<Application>(documentStore);
             //}
 
-            using (SimpleProfiler.Start("Benchmark"))
+            using (MiniProfiler.Current.Step("Create indexes"))
             {
-                using (SimpleProfiler.Start("Creating indexes"))
-                {
-                    CreateIndexes(documentStore, IndexCount);
-                }
-
-                using (SimpleProfiler.Start("Creating documents"))
-                {
-                    CreateDocuments(documentStore, DocumentCount);
-                }
-
-                using (SimpleProfiler.Start("Waiting for stale indexes"))
-                {
-                    WaitForStaleIndexes(documentStore);
-                }
-
-                using (SimpleProfiler.Start("Querying for documents"))
-                {
-                    QueryForDocuments(documentStore, IndexCount);
-                }
+                CreateIndexes(documentStore, indexCount);
             }
+
+            using (MiniProfiler.Current.Step("Creating documents"))
+            {
+                CreateDocuments(documentStore, documentCount);
+            }
+
+            using (MiniProfiler.Current.Step("Waiting for stale indexes"))
+            {
+                WaitForStaleIndexes(documentStore);
+            }
+
+            using (MiniProfiler.Current.Step("Querying for documents"))
+            {
+                QueryForDocuments(documentStore, indexCount);
+            }
+
+            MiniProfiler.Stop();
+
+            var report =
+                Environment.CommandLine + Environment.NewLine +
+                MiniProfiler.Current.RenderPlainText() + Environment.NewLine + Environment.NewLine;
+            Console.WriteLine(report);
+            File.AppendAllText("output.txt", report);
 
             while (true)
             {
-                Console.WriteLine("sleeping");
+                Console.WriteLine("Sleeping. You can open localhost:8080.");
                 Thread.Sleep(30000);
             }
         }
@@ -126,11 +157,13 @@ namespace RavenDbPerformanceTest
                 string firstName = firstNames[r.Next(firstNames.Length)];
                 string lastName = firstNames[r.Next(lastNames.Length)];
 
-                var client = new Client(firstName, lastName, DateTime.Now - TimeSpan.FromDays(r.Next(30, 20000)));
+                var clientBirthday = DateTime.Now - TimeSpan.FromDays(r.Next(30, 20000));
+                var client = new Client(firstName, lastName, clientBirthday);
                 session.Store(client);
                 session.SaveChanges();
 
-                var app = new Application(client.Id, DateTime.Now - TimeSpan.FromDays(r.Next(2, 100)), RandomStringGenerator.RandomString(340));
+                var applicationDate = DateTime.Now - TimeSpan.FromDays(r.Next(2, 100));
+                var app = new Application(client.Id, applicationDate, RandomStringGenerator.RandomString(340));
                 session.Store(app);
                 session.SaveChanges();
             }
@@ -146,23 +179,31 @@ namespace RavenDbPerformanceTest
 
         public static void QueryForDocuments(IDocumentStore documentStore, int indexCount)
         {
-            var firstFortyLastNames = File.ReadAllLines(Path.Combine("Names", "LastNames.txt")).Take(40).ToArray();
+            var first40LastNames = File.ReadAllLines(Path.Combine("Names", "LastNames.txt")).Take(40).ToArray();
+            var first200FirstNames = File.ReadAllLines(Path.Combine("Names", "FirstNames.txt")).Take(200).ToArray();
 
             for (int i = 1; i <= indexCount; i++)
             {
-                Stopwatch sw = Stopwatch.StartNew();
-
                 var indexName = string.Format("{0}{1}", typeof(ClientApplication).Name, i);
+
                 var docs = documentStore.OpenSession().Query<ClientApplication.IndexResult>(indexName)
                     .Where(x => x.BirthDate < new DateTime(1990, 1, 1) ||
                                 x.Data.StartsWith("A") ||
                                 x.FirstName == "Bernardo" ||
-                                x.LastName.In(firstFortyLastNames))
+                                x.LastName.In(first40LastNames))
                     .Take(10000)
                     .ToList();
 
-                sw.Stop();
-                Console.WriteLine("{0} documents queried from {1} in {2} ms", docs.Count, indexName, sw.ElapsedMilliseconds);
+                Console.WriteLine("{0} documents", docs.Count);
+
+                var docs2 = documentStore.OpenSession().Query<ClientApplication.IndexResult>(indexName)
+                    .Where(x => (x.BirthDate < new DateTime(2000, 1, 1) && x.BirthDate > new DateTime(1990, 1, 1)) ||
+                                x.FirstName.In(first200FirstNames) &&
+                                x.LastName != null)
+                    .Take(10000)
+                    .ToList();
+
+                Console.WriteLine("{0} documents", docs2.Count);
             }
         }
     }
